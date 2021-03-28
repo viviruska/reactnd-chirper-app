b20df289d68fb6a27609cdd0ef28a74e46d3c52b
viviruska <vivien.ruska@prezi.com>
2021-03-28 19:28:24 +0200

    feat: Add Tweet placeholder to see whether data is coming in

---
+++ src/components/Tweet.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { formatTweet } from '../utils/helpers'
+
+class Tweet extends Component {
+  render() {
+      console.log(this.props)
+    return (
+      <div className='tweet'>
+
+      </div>
+    )
+  }
+}
+
+function mapStateToProps({authedUser, users, tweets}, { id }) {
+  const tweet = tweets[id]
+
+  return {
+    authedUser,
+    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+  }
+}
+
+export default connect(mapStateToProps)(Tweet)
