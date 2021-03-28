ce341e6e67d4e04b415127ee259fa8021b83c6f4
viviruska <vivien.ruska@prezi.com>
2021-03-28 19:54:34 +0200

    feat: Add all of the data needed for Tweet component

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -4,7 +4,13 @@ import { formatTweet } from '../utils/helpers'
 
 class Tweet extends Component {
   render() {
-      console.log(this.props)
+    const { tweet } = this.props
+    
+    if (tweet === null) {
+      return <p>This Tweet doesn't exist</p>
+    }
+    console.log(this.props)
+    
     return (
       <div className='tweet'>
 
@@ -15,10 +21,13 @@ class Tweet extends Component {
 
 function mapStateToProps({authedUser, users, tweets}, { id }) {
   const tweet = tweets[id]
+  const parentTweet = tweet ? tweets[tweet.replyingTo] : null
 
   return {
     authedUser,
-    tweet: formatTweet(tweet, users[tweet.author], authedUser)
+    tweet: tweet
+      ? formatTweet(tweet, users[tweet.author], authedUser, parentTweet)
+      : null
   }
 }
 
