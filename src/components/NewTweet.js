9d74fe26319391b03e66f2bcd5bdfaf7ffc83773
viviruska <vivien.ruska@prezi.com>
2021-04-01 20:27:34 +0200

    feat: Add reducer for adding tweet

--- src/components/NewTweet.js
+++ src/components/NewTweet.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { handleAddTweet } from '../actions/tweets'
 
 class NewTweet extends Component {
   state = {
@@ -15,8 +17,9 @@ class NewTweet extends Component {
     e.preventDefault()
 
     const { text } = this.state
-    // todo: Add Tweet to Store
-    console.log('New Tweet: ', text)
+    const { dispatch, id } = this.props
+
+    dispatch(handleAddTweet(text, id))
 
     this.setState(() => ({
       text: ''
@@ -56,4 +59,4 @@ class NewTweet extends Component {
   }
 }
 
-export default NewTweet
+export default connect()(NewTweet)
